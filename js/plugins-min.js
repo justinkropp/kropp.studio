18636f6c695c6e7e4a492d50fa3489b9018920dd
justin kropp <justinkropp@gmail.com>
2020-12-30 09:59:59 -0500

    adding base html files

---
+++ js/plugins-min.js
@@ -0,0 +1,5 @@
+!function(){"use strict";
+// Avoid `console` errors in browsers that lack a console.
+!function(){for(var e,n=function(){},i=["assert","clear","count","debug","dir","dirxml","error","exception","group","groupCollapsed","groupEnd","info","log","markTimeline","profile","profileEnd","table","time","timeEnd","timeline","timelineEnd","timeStamp","trace","warn"],o=i.length,r=window.console=window.console||{};o--;)
+// Only stub undefined methods.
+r[e=i[o]]||(r[e]=n)}()}();
